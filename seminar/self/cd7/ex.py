78a2779539dc33bc21b9bb9db4deffc1ec627af9
sive2045 <lc9902130509@gmail.com>
2021-09-07 20:57:42 +0900

    update data

--- seminar/self/cd7/ex.py
+++ seminar/self/cd7/ex.py
@@ -3,10 +3,10 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from common.util import im2col
 import numpy as np
 
-x1 = np.random.rand(1,3,7,7)
-col1 = im2col(x1, 5, 5, stride=1, pad=0)
-print(col1.shape)
+x1 = np.random.rand(1,3,7,7) # (데이터 수, 채널 수, 높이, 너비)
+col1 = im2col(x1, 5, 5, stride=1, pad=0) # 채널 수는 입력 데이터와 같음
+print(col1.shape)# (9, 75)
 
-x2 = np.random.rand(10, 3, 7, 7)
+x2 = np.random.rand(10, 3, 7, 7) # 데이터 10개
 col2 = im2col(x2, 5, 5, stride=1, pad=0)
-print(col2.shape)
+print(col2.shape) # (90, 75)
